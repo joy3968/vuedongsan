c2f39ad31f410e6a183fc448d3171d0cb3af9caf
minwoo <sasyo1z2@naver.com>
2023-02-13 01:42:36 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+  </div>
+
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+    <!-- v-on: 대신 @로 대체할 수 있음. -->
+    <button @click="increase">허위매물신고</button>
+    <span>신고수 : {{ 신고수 }}
+    </span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      신고수 : 0,
+      price1 : 67,
+      price2 : 70,
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods: {
+    increase() {
+      this.신고수 += 1;
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
