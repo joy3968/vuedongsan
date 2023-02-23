74a175e9000b8e671297ac089c1a810692977603
minwoo <sasyo1z2@naver.com>
2023-02-23 23:24:02 +0900

    신고버튼 분리 및, 이미지 추가

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,29 @@
   </div>
 
   <div>
+    <img src="./assets/room0.jpg" class="room-img"/>
     <h4>{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
     <!-- v-on: 대신 @로 대체할 수 있음. -->
-    <button @click="increase">허위매물신고</button>
-    <span>신고수 : {{ 신고수 }}
+    <button @click="increase(0)">허위매물신고</button>
+    <span> 신고수 : {{ 신고수[0] }}
     </span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img"/>
     <h4>{{ products[1] }}</h4>
     <p>{{ price2 }} 만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span> 신고수 : {{ 신고수[1] }}
+    </span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img"/>
     <h4>{{ products[2] }}</h4>
-    <p>{{ price2 }} 만원</p>
+    <p>{{ price3 }} 만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span> 신고수 : {{ 신고수[2] }}
+    </span>
   </div>
 </template>
 
@@ -27,16 +36,17 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수 : 0,
+      신고수 : [0, 0, 0],
       price1 : 67,
       price2 : 70,
+      price3 : 80,
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
   methods: {
-    increase() {
-      this.신고수 += 1;
+    increase(i) {
+      this.신고수[i] += 1;
     }
   },
   components: {
@@ -45,6 +55,11 @@ export default {
 </script>
 
 <style>
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
